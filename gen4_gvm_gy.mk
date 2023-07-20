123eb754c2339e44077cf51c012a85852548c2c3
satbir singh <quic_satbsing@quicinc.com>
2023-07-20 10:36:45 -0400

    gen4_gvm_gy: Enabling the display proxy service
    Adding the display proxy service in the PRODUCT_PACKAGES.
    Needed by the EVS.
    
    Change-Id: Ie3c1c52a6aa695b136f42c63c386679119e05343
    Signed-off-by: satbir singh <quic_satbsing@quicinc.com>

--- gen4_gvm_gy.mk
+++ gen4_gvm_gy.mk
@@ -42,6 +42,8 @@ ifeq ($(KERNEL_MODULES_OUT),)
   KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_DEVICE)/$(KERNEL_MODULES_INSTALL)/lib/modules
 endif
 
+PRODUCT_PACKAGES += android.frameworks.automotive.display@1.0-service
+
 # TARGET_KERNEL_VERSION := 5.15
 # TARGET_HAS_GENERIC_KERNEL_HEADERS := true
 # Set the system.prop files to that of the inherited product plus the new product. Alternatively, the system.prop file can be copied over
