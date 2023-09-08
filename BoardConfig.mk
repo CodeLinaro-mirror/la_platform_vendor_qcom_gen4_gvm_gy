2e2bf6d68dd47727d77654df1a36a72e5f4857f4
hrose <quic_hrose@quicinc.com>
2023-09-08 10:49:06 +0530

    gen4_gvm_gy : gen4_gvm_gy build fixes
    Keep AVB settings, OTA partitions same as qssi,
    aidl versioning fixes in manifest
    
    Signed-off-by: hrose <quic_hrose@quicinc.com>
    Change-Id: Ib4f3475c98ffbf9b8ac94dafe63efe3b3aaceb16

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,13 +2,6 @@
 include device/qcom/gen4_gvm/BoardConfig.mk
 
 
-#If AVB is disabled need to override AB_OTA_PARTITIONS so that vbmeta is not part of
-#OTA partiotion
-ifeq ($(ENABLE_AB), true)
-  AB_OTA_PARTITIONS := system system_ext vendor vendor_dlkm system_dlkm
-endif
-
-BOARD_AVB_ENABLE := false
 
 DEVICE_MANIFEST_FILE := device/qcom/gen4_gvm_gy/manifest.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/gen4_gvm_gy/framework_manifest.xml
