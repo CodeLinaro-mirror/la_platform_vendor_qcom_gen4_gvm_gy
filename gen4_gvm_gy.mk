1fdc4ac750fe7d68420ada4e606442558b1553c8
Vijayakumar Badiger <vbadiger@qti.qualcomm.com>
2025-11-08 09:25:56 -0800

    gen4_gvm_gy: Bringup changes for A-16
    Port changes needed to A-16 bring up.
    
    Change-Id: Ia1e4f4ef6df8966c5800d24ba298ab396b7ce5c6
    Signed-off-by: Vijayakumar Badiger <vbadiger@qti.qualcomm.com>

--- gen4_gvm_gy.mk
+++ gen4_gvm_gy.mk
@@ -25,7 +25,7 @@ BOARD_HAS_QCOM_WLAN := false
 ENABLE_AB ?= true
 
 ifeq ($(ENABLE_AB), true)
-PRODUCT_COPY_FILES += device/qcom/gen4_gvm_gy/fstab_AB_dynamic_partition_variant.gen4_gy.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+PRODUCT_COPY_FILES += device/qcom/gen4_gvm_gy/fstab_AB_dynamic_partition_variant.gen4_gy.qti:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.qcom
 endif
 
 
@@ -49,18 +49,15 @@ TARGET_USES_QMAA_OVERRIDE_DISPLAY := true
 #Full QMAA HAL List
 QMAA_HAL_LIST := audio
 
-# Change Kernel modules install path
+# Kernel modules install path
 KERNEL_MODULES_INSTALL := dlkm
-ifeq ($(KERNEL_MODULES_OUT),)
-  KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_DEVICE)/$(KERNEL_MODULES_INSTALL)/lib/modules
-endif
+
+KERNEL_MODULES_OUT := out/target/product/$(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)$(TARGET_BOARD_DERIVATIVE_SUFFIX)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
 
 
 PRODUCT_PACKAGES += android.frameworks.automotive.display@1.0-service
 
-# TARGET_KERNEL_VERSION := 5.15
-# TARGET_HAS_GENERIC_KERNEL_HEADERS := true
-# Set the system.prop files to that of the inherited product plus the new product. Alternatively, the system.prop file can be copied over
 
 PRODUCT_VENDOR_PROPERTIES += \
     ro.boot.audio=audioreach_vio
